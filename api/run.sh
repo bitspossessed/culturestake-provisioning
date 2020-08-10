13ce6630b44f84bcbed63daeef991d2470701b08
anon <sarah.anne.friend@gmail.com>
2020-08-10 11:25:23 -0400

    Add app to production

--- api/run.sh
+++ api/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Wait for postgres to be ready
-while ! PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE_API" > /dev/null 2> /dev/null; do
+while ! PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE_MAIN" > /dev/null 2> /dev/null; do
   echo "Waiting for database to be ready ..."
   sleep 5
 done
 
 >&2 echo "Database is ready!"
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DATABASE_API
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DATABASE_MAIN
 export DATABASE_DIALECT=postgres
 
 export NODE_ENV=production
@@ -16,4 +16,4 @@ export NODE_ENV=production
 npm run db:migrate
 npm run db:seed
 
-node ./build/index.js
+node ./build/server/index.js
